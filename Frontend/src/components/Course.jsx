19d2a325531d31a8c288f2b2667f76f88af0792b
Yash Singh <yashxsingh16@gmail.com>
2025-01-11 15:48:50 +0530

    DeployTRY

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -7,7 +7,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get(`${import.meta.env.REACT_API_URL}/book`);
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
