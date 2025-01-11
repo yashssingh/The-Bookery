be924bb211deb9a715f5738c916353786e5d6029
Yash Singh <yashxsingh16@gmail.com>
2025-01-11 16:21:26 +0530

    DeployFIX

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "class",
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 };
